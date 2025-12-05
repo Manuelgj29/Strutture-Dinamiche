4fad2bb15e1637f7fc708b2c139ee4c6ceeea45a
fsannicolo <federico.sannicolo@marconirovereto.it>
2025-12-05 14:55:24 +0100

    first commit

---
+++ src/CustomList.java
@@ -0,0 +1,27 @@
+public class CustomList {
+    
+    private Node head;
+
+    public CustomList() {
+        this.head = null;
+    }
+
+    public CustomList(Node newHead) {
+        this.head = newHead;
+    }
+
+    /**
+     * Adds a new node at the end of the list
+     * @param newNode new node to be added
+     */
+    public void append(Node newNode) {
+
+    }
+
+    /**
+     * Outputs every list item on the console
+     */
+    public void print() {
+
+    }
+}
