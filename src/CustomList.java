2e6d24023b3bc3ee536134a872b3c6978d8e3579
Manuel Gjergji <manuel.gjergji@marconirovereto.it>
2025-12-11 09:57:58 +0000

    Refactor append and print methods in CustomList for improved functionality and clarity

--- src/CustomList.java
+++ src/CustomList.java
@@ -15,13 +15,37 @@ public class CustomList {
      * @param newNode new node to be added
      */
     public void append(Node newNode) {
-
+        if (head == null) {
+            // If the list is empty, the new node becomes the head
+            head = newNode;
+        } else {
+            // Otherwise, traverse to the last node and append
+            Node current = head;
+            while (current.getNext() != null) {
+                current = current.getNext();
+            }
+            current.setNext(newNode);
+        }
     }
 
     /**
      * Outputs every list item on the console
      */
     public void print() {
-
+        if (head == null) {
+            System.out.println("List is empty");
+            return;
+        }
+        
+        Node current = head;
+        System.out.print("List: ");
+        while (current != null) {
+            System.out.print(current.toString());
+            if (current.getNext() != null) {
+                System.out.print(" -> ");
+            }
+            current = current.getNext();
+        }
+        System.out.println();
     }
 }
