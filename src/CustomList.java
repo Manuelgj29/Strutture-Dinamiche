83a7afee7f9690861e7f6de34fe0c1118cfeb642
Manuel Gjergji <manuel.gjergji@marconirovereto.it>
2025-12-15 10:53:28 +0000

    Refactor CustomList methods for improved clarity and functionality

--- src/CustomList.java
+++ src/CustomList.java
@@ -1,4 +1,4 @@
-public class CustomList {
+ public class CustomList {
     
     private Node head;
 
@@ -10,16 +10,11 @@ public class CustomList {
         this.head = newHead;
     }
 
-    /**
-     * Adds a new node at the end of the list
-     * @param newNode new node to be added
-     */
+    
     public void append(Node newNode) {
         if (head == null) {
-            // If the list is empty, the new node becomes the head
             head = newNode;
         } else {
-            // Otherwise, traverse to the last node and append
             Node current = head;
             while (current.getNext() != null) {
                 current = current.getNext();
@@ -28,9 +23,6 @@ public class CustomList {
         }
     }
 
-    /**
-     * Outputs every list item on the console
-     */
     public void print() {
         if (head == null) {
             System.out.println("List is empty");
@@ -47,5 +39,54 @@ public class CustomList {
             current = current.getNext();
         }
         System.out.println();
+    }
+
+    public void Add(int index, Node newNode) throws indexOutOfBoundsException{
+        if(index == 0){
+            newNode.setNext(head);
+            this.head = newNoad;
+        }esle if(index > 0 && index <= size()){
+            Node prev = get(index-1);
+            newNode.setNext(prev.getNext());
+            prev.setNext(newNode);
+        }else
+            throw new indexOutOfBoundsException("invalid position");
+    }
+
+    //metodoGet che ti ritorna il nodo che vuoi
+    private Node getRec(Node cursor, int index) throws indexOutOfBoundsException{
+        if(index < 0 || index >= size()){
+            throw new indexOutOfBoundsException("invalid index");
+        }
+        if(index == 0)
+            return head;
+
+        return getRec (cursor.getNext(), index - 1)
+    }
+
+    public Node get (int index){
+        return getRec(head, index);
+    }
+
+    // Lunghezza del nodo in modo ricorsivo
+    public int size(){
+        return sizeRE(head);
+    }
+
+    private int sizeRE(Node cursor){
+        if(cursor == null) return 0;
+
+        return sizeRE(cursor.getNext()) +1;
+    }
+    
+    // Lunghezza del nodo
+    public int length(){
+        Node cursor = head;
+        int count = 0;
+        while(cursor != null){
+            count++;
+            cursor = cursor.getNext();       
+        }
+        return count;
     }
 }
